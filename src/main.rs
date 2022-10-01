4df05172dfde3612d52724923f4838b9e94c85ba
nuts3745 <japanbik@gmail.com>
2022-10-01 18:12:52 +0900

    HashMapの例

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 fn main() {
-    let message = if true {
-        "hello, foo"
-    } else {
-        "hello, bar"
-    };
+    let message = if true { "hello, foo" } else { "hello, bar" };
     println!("{}", message);
+
+    let mut scores = std::collections::HashMap::new();
+    scores.insert("Sato", 100);
+    scores.insert("Tanaka", 90);
+    scores.entry("Tanaka").or_insert(100);
+    println!("{:?}", scores);
+
+    let solar_distance = std::collections::HashMap::from([
+        ("Mercury", 0.4),
+        ("Venus", 0.7),
+        ("Earth", 1.0),
+        ("Mars", 1.5),
+    ]);
+    println!("{:?}", solar_distance);
 }
