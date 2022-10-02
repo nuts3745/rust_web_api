00393e6190dded64e7b7eff6d1ab256e0c064f0e
nuts3745 <japanbik@gmail.com>
2022-10-02 16:04:48 +0900

    戻り値のない関数

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
+fn void() {
+    
+}
+
+fn return_void() {
+    return ();
+}
+
 fn main() {
-    let message = if true { "hello, foo" } else { "hello, bar" };
-    println!("{}", message);
-
-    let mut scores = std::collections::HashMap::new();
-    scores.insert("Sato", 100);
-    scores.insert("Tanaka", 90);
-    scores.entry("Tanaka").or_insert(100);
-    println!("{:?}", scores);
-
-    let solar_distance = std::collections::HashMap::from([
-        ("Mercury", 0.4),
-        ("Venus", 0.7),
-        ("Earth", 1.0),
-        ("Mars", 1.5),
-    ]);
-    println!("{:?}", solar_distance);
+    let a = void();
+    println!("{:?}", a);
+    let a = return_void();
+    println!("{:?}", a);
 }
