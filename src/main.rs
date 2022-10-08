9bd0094c3739201af1778bc0b76fb8b7ddeca596
nuts3745 <japanbik@gmail.com>
2022-10-08 17:07:25 +0900

    enum

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,18 @@ fn add(x: i32, y: i32) -> i32 {
     x + y
 }
 
+enum Color {
+    Red,
+    Blue,
+    Green,
+    Hex(String),
+}
+
 fn main() {
     println!("{}", add(1, 2));
     let a = if true { 10 } else { 20 };
     let b = { 10 * 2 };
     println!("{} {}", a, b);
+    let red = Color::Red;
+    let hex = Color::Hex("ffffff".to_string());
 }
