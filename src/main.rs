f30315b1e8c34cd7d863301ba273f361a19eaa3c
nuts3745 <japanbik@gmail.com>
2022-10-08 22:15:12 +0900

    パターンマッチ

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,33 @@
-fn main() {
-    if true {
-        println!("inner");
+fn fizz_buzz(value: i32) -> String {
+    let result = match value {
+        v if v % 15 == 0 => "fizz buzz".to_string(),
+        v if v % 5 == 0 => "buzz".to_string(),
+        v if v % 3 == 0 => "fizz".to_string(),
+        _ => value.to_string(),
     };
-    println!("outer");
+    result
+}
+
+enum Color {
+    Red,
+    Blue,
+    Green,
+    Hex(String),
+}
+
+fn string_to_color_token(value: &str) -> Option<Color> {
+    match value {
+        "red" => Some(Color::Red),
+        "blue" => Some(Color::Blue),
+        "green" => Some(Color::Green),
+        "white" => Some(Color::Hex("f8f7f2".to_string())),
+        _ => None,
+    }
+}
+
+fn main() {
+    println!("{}", fizz_buzz(1));
+    println!("{}", fizz_buzz(3));
+    println!("{}", fizz_buzz(5));
+    let result = string_to_color_token("red").unwrap();
 }
