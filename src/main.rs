e02d457b8b3e9dbb1bd4caa52eeb418206f3ca57
nuts3745 <japanbik@gmail.com>
2022-10-08 18:12:56 +0900

    ?をつけてエラーを伝播

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
+fn always_error() -> Result<(), String> {
+    Err("常にエラーが発生します".to_string())
+}
+
+fn might_fail() -> Result<(), String> {
+    let _result = always_error()?;
+    Ok(())
+}
+
 fn main() {
-    let some: Result<&str, &str> = Ok("ok");
-    println!("{:?}", some);
-    let err: Result<&str, &str> = Err("err");
-    println!("{:?}", err);
+    let message = match might_fail() {
+        Ok(_) => "処理に成功しました".to_string(),
+        Err(cause_message) => cause_message,
+    };
+    println!("{}", message);
 }
