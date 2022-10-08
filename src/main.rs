61eb8a749175fa849abd3387a5fa11400beff612
nuts3745 <japanbik@gmail.com>
2022-10-08 22:58:24 +0900

    while式

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 fn add_until(start: i32, end: i32) -> i32 {
     let mut sum = 0;
     let mut temp = start;
-    loop {
+    while temp <= end {
         sum += temp;
-        if temp == end {
-            break sum;
-        }
         temp += 1;
     }
+    sum
 }
 
 fn main() {
