069d6d8aec5306f6de0eb8408d4257f71144d24c
nuts3745 <japanbik@gmail.com>
2022-10-08 21:19:44 +0900

    ドキュメンテーションコメントの書き方

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
+/// xとyを足し合わせます。
+///
+/// # Examples
+///
+/// ```
+/// use chapter_01::utils::add;
+///
+/// let r = add(1, 10);
+/// assert_eq!(11, r);
+/// ```
+pub fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+
 fn main() {
-    let input: Result<&str, &str> = Ok("test");
-    let input = input.unwrap();
-    println!("{:?}", input);
+    println!("{}", add(1, 2))
 }
