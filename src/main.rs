a2b092aaa7e23c06b90168c8f27af28549e497a3
nuts3745 <japanbik@gmail.com>
2022-10-08 21:48:48 +0900

    戻り値使わないならif文っぽく書ける

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,6 @@
-fn fizz_buzz(value: i32) -> String {
-    let result = if value % 15 == 0 {
-        "fizz buzz".to_string()
-    } else if value % 5 == 0 {
-        "buzz".to_string()
-    } else if value % 3 == 0 {
-        "fizz".to_string()
-    } else {
-        value.to_string()
-    };
-    result
-}
-
 fn main() {
-    println!("{}", fizz_buzz(1));
-    println!("{}", fizz_buzz(3));
-    println!("{}", fizz_buzz(5));
+    if true {
+        println!("inner");
+    };
+    println!("outer");
 }
