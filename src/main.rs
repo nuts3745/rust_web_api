973d6bb3342938d43510e229c0813745be342a1a
nuts3745 <japanbik@gmail.com>
2022-10-08 17:29:37 +0900

    Resultは回復可能なエラーを表す

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,6 @@
-fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-enum Color {
-    Red,
-    Blue,
-    Green,
-    Hex(String),
-}
-
 fn main() {
-    println!("{}", add(1, 2));
-    let a = if true { 10 } else { 20 };
-    let b = { 10 * 2 };
-    println!("{} {}", a, b);
-    let red = Color::Red;
-    let hex = Color::Hex("ffffff".to_string());
+    let some: Result<&str, &str> = Ok("ok");
+    println!("{:?}", some);
+    let err: Result<&str, &str> = Err("err");
+    println!("{:?}", err);
 }
