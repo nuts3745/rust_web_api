6af6fcb45f89f6a6bb8ba6b87579da0c3eafc3d1
nuts3745 <japanbik@gmail.com>
2022-10-08 21:46:30 +0900

    if式

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
-macro_rules! sum {
-    ($($x:expr),*) => {
-        {
-            let mut result = 0;
-            $(
-                result = result + $x;
-            )*
-            result
-        }
+fn fizz_buzz(value: i32) -> String {
+    let result = if value % 15 == 0 {
+        "fizz buzz".to_string()
+    } else if value % 5 == 0 {
+        "buzz".to_string()
+    } else if value % 3 == 0 {
+        "fizz".to_string()
+    } else {
+        value.to_string()
     };
+    result
 }
 
 fn main() {
-    println!("{}", sum![1, 2, 3, 4, 5]);
+    println!("{}", fizz_buzz(1));
+    println!("{}", fizz_buzz(3));
+    println!("{}", fizz_buzz(5));
 }
