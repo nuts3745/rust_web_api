4a90c1c000fed2c9df2ce0ffe21a9249ff6a8cd4
nuts3745 <japanbik@gmail.com>
2022-10-08 18:26:22 +0900

    panic!マクロを利用した回復不能なエラー

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-fn always_error() -> Result<(), String> {
-    Err("常にエラーが発生します".to_string())
-}
-
-fn might_fail() -> Result<(), String> {
-    let _result = always_error()?;
-    Ok(())
-}
-
 fn main() {
-    let message = match might_fail() {
-        Ok(_) => "処理に成功しました".to_string(),
-        Err(cause_message) => cause_message,
-    };
-    println!("{}", message);
+    println!("before panic!");
+    panic!("hoge");
+    println!("after panic!");
 }
