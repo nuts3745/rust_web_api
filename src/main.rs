af6d4654ed470308bf5dded834c1a76e2232cf79
nuts3745 <japanbik@gmail.com>
2022-10-09 23:58:05 +0900

    async/await

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,7 @@
-use std::fmt::Display;
+use warp::Filter;
 
-trait Area {
-    fn area(&self) -> u32;
-}
-
-struct Square(u32);
-
-impl Area for Square {
-    fn area(&self) -> u32 {
-        self.0.pow(2)
-    }
-}
-
-impl Square {
-    fn new(side: u32) -> Self {
-        Self(side)
-    }
-}
-
-struct Rectangle(u32, u32);
-
-impl Area for Rectangle {
-    fn area(&self) -> u32 {
-        self.0 * self.1
-    }
-}
-
-impl Rectangle {
-    fn new(a: u32, b: u32) -> Self {
-        Self(a, b)
-    }
-}
-
-fn comparison_area<T, U>(a: T, b: U) -> bool
-where
-    T: Area + Display,
-    U: Area + Display,
-{
-    a.area() == b.area()
-}
-
-fn main() {
-    let my_square = Square::new(5);
-    let my_rectangle = Rectangle::new(5, 5);
-    if comparison_area(my_square, my_rectangle) {
-        println!("面積は等しい");
-    } else {
-        println!("面積は異なる");
-    }
+#[tokio::main]
+async fn main() {
+    let hello = warp::path::end().map(|| format!("Hello, warp!"));
+    warp::serve(hello).run(([127, 0, 0, 1], 3030)).await;
 }
