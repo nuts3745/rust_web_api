03dafa2c6a55f94cf49ef81a2ad712be8cf6306f
nuts3745 <japanbik@gmail.com>
2022-10-09 22:25:35 +0900

    自前のトレイトを型に実装する例

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,22 @@
-use std::fmt::{self, Display};
-
-struct User {
-    name: String,
-    age: u32,
+trait Area {
+    fn area(&self) -> u32;
 }
 
-impl Display for User {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "user name is {}, age is {}", &self.name, &self.age)
+struct Square(u32);
+
+impl Area for Square {
+    fn area(&self) -> u32 {
+        self.0.pow(2)
     }
 }
 
-impl User {
-    fn new(name: String, age: u32) -> Self {
-        User { name, age }
-    }
-
-    fn rename(&mut self, name: String) {
-        self.name = name;
+impl Square {
+    fn new(side: u32) -> Self {
+        Self(side)
     }
 }
-
-struct StraightLine(i32, i32);
 
 fn main() {
-    let mut user = User::new("Sato".to_string(), 30);
-    println!("{}", user);
-    user.rename(String::from("Kato"));
-    println!("{}", user);
-    let line: StraightLine = StraightLine(12, 12);
-    println!("{} {}", line.0, line.1);
+    let my_square = Square::new(5);
+    println!("area is {}", my_square.area());
 }
