37274f89e2cc8831275e87417d47a799c75b7720
nuts3745 <japanbik@gmail.com>
2022-10-09 22:07:03 +0900

    メソッドと関連関数

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,27 @@ struct User {
     age: u32,
 }
 
+impl User {
+    fn new(name: String, age: u32) -> Self {
+        Self { name, age }
+    }
+
+    fn rename(&mut self, name: String) {
+        self.name = name;
+    }
+
+    fn description(&self) -> String {
+        format!("user name is {}, age is {}", &self.name, &self.age)
+    }
+}
+
 struct StraightLine(i32, i32);
 
 fn main() {
-    let user = User{
-        name: "Sato".to_string(),
-        age: 30,
-    };
-    println!("name is {}, age is {}", user.name, user.age);
+    let mut user = User::new("Sato".to_string(), 30);
+    println!("{}", user.description());
+    user.rename(String::from("Kato"));
+    println!("{}", user.description());
     let line: StraightLine = StraightLine(12, 12);
     println!("{} {}", line.0, line.1);
 }
