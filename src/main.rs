4810f4fc2bcea6ba35f6d7f086fb98803e8e8404
nuts3745 <japanbik@gmail.com>
2022-10-09 22:37:48 +0900

    分数を表すFraction型にAddトレイトを実装して足し算をできるようにする例

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,49 @@
-trait Area {
-    fn area(&self) -> u32;
-}
+use std::fmt::{self, Display};
+use std::ops::Add;
 
-struct Square(u32);
+struct Fraction(u32, u32);
 
-impl Area for Square {
-    fn area(&self) -> u32 {
-        self.0.pow(2)
+impl Fraction {
+    fn new(numerator: u32, denominator: u32) -> Self {
+        let gcf_value = Self::gcf(numerator, denominator);
+        Self(numerator / gcf_value, denominator / gcf_value)
+    }
+
+    fn gcf(value1: u32, value2: u32) -> u32 {
+        let (mut a, mut b) = if value1 > value2 {
+            (value1, value2)
+        } else {
+            (value2, value1)
+        };
+        let mut r = a % b;
+        while r != 0 {
+            a = b;
+            b = r;
+            r = a % b;
+        }
+        b
     }
 }
 
-impl Square {
-    fn new(side: u32) -> Self {
-        Self(side)
+impl Display for Fraction {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}/{}", &self.0, &self.1)
+    }
+}
+
+impl Add for Fraction {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        let lcm = self.1 / Self::gcf(self.1, rhs.1) * rhs.1;
+        let a = self.0 * (lcm / self.1);
+        let b = rhs.0 * (lcm / rhs.1);
+        Fraction::new(a + b, lcm)
     }
 }
 
 fn main() {
-    let my_square = Square::new(5);
-    println!("area is {}", my_square.area());
+    let a = Fraction::new(8, 18);
+    let b = Fraction::new(1, 2);
+    println!("{}", a + b);
 }
