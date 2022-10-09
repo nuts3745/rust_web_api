3ee64aaff13b51b02f7e5ed75e12c0f12c2de0ef
nuts3745 <japanbik@gmail.com>
2022-10-09 22:19:34 +0900

    DisplayをUserに実装してみるトレイトの例

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
+use std::fmt::{self, Display};
+
 struct User {
     name: String,
     age: u32,
 }
 
+impl Display for User {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "user name is {}, age is {}", &self.name, &self.age)
+    }
+}
+
 impl User {
     fn new(name: String, age: u32) -> Self {
-        Self { name, age }
+        User { name, age }
     }
 
     fn rename(&mut self, name: String) {
         self.name = name;
-    }
-
-    fn description(&self) -> String {
-        format!("user name is {}, age is {}", &self.name, &self.age)
     }
 }
 
@@ -21,9 +25,9 @@ struct StraightLine(i32, i32);
 
 fn main() {
     let mut user = User::new("Sato".to_string(), 30);
-    println!("{}", user.description());
+    println!("{}", user);
     user.rename(String::from("Kato"));
-    println!("{}", user.description());
+    println!("{}", user);
     let line: StraightLine = StraightLine(12, 12);
     println!("{} {}", line.0, line.1);
 }
