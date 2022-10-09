c6d6561660e9fec303b9c31b98718d70ddd48810
nuts3745 <japanbik@gmail.com>
2022-10-09 12:51:14 +0900

    構造体（名前付きフィールド、タプル構造体）

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+struct User {
+    name: String,
+    age: u32,
+}
+
+struct StraightLine(i32, i32);
+
 fn main() {
-    let a = Box::new("test");
-    println!("{}", a);
+    let user = User{
+        name: "Sato".to_string(),
+        age: 30,
+    };
+    println!("name is {}, age is {}", user.name, user.age);
+    let line: StraightLine = StraightLine(12, 12);
+    println!("{} {}", line.0, line.1);
 }
