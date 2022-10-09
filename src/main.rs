3ba3550469c0273e13636729e66e17a16d865232
nuts3745 <japanbik@gmail.com>
2022-10-09 12:36:42 +0900

    Box

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,4 @@
-fn hello() -> &'static str {
-    "hello"
-}
-
-fn print_hello<'a>(name: &'a str) {
-    println!("hello, {}", name);
-}
-
 fn main() {
-    let a: &'static str = "some text";
-    println!("{}", hello());
-    print_hello(a);
+    let a = Box::new("test");
+    println!("{}", a);
 }
