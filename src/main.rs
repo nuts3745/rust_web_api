af38bd5079a8ed9fb3b6b3e2ad720b12440ed85a
nuts3745 <japanbik@gmail.com>
2022-10-10 13:35:41 +0900

    ユニットテスト

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
-mod foo;
-
 fn main() {
-    foo::bar::baz();
+    println!("{}", add(12, 21));
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct Book {
+    name: String
+}
+/// xとyを足し合わせます。
+/// 
+/// # Example
+/// 
+/// ```
+/// use chapter_02::utils::add;
+/// 
+/// let r = add(1, 10);
+/// assert_eq!(11, r);
+/// ```
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn add_test() {
+        assert_eq!(2, add(1, 1));
+    }
+
+    #[test]
+    fn book_test() {
+        let book1 = Book {
+            name: "book name".to_string()
+        };
+        let book2 = Book {
+            name: "book name".to_string()
+        };
+        assert_eq!(book1, book2);
+    }
+
+    #[test]
+    fn return_result_test() -> Result<(), ()> {
+        Ok(())
+    }
 }
