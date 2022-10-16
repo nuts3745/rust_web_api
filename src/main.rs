f988461fd37ab70c473319d41069ba39508f3d61
nuts3745 <japanbik@gmail.com>
2022-10-16 11:56:01 +0900

    todoアプリを作る

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,3 @@
-use serde::{Deserialize, Serialize};
-
-#[derive(Serialize, Deserialize, Debug)]
-struct User {
-    name: String,
-    age: u32,
-}
-
 fn main() {
-    let user = User {
-        name: String::from("sato"),
-        age: 30,
-    };
-
-    let serialized = serde_json::to_string(&user).unwrap();
-    println!("serialized = {}", serialized);
-
-    let deserialized: User = serde_json::from_str(&serialized).unwrap();
-    println!("deserialized = {:?}", deserialized);
+    println!("Hello, world!");
 }
