8e5c244478c80c587ef66cbaaa4fc3ea5a37442f
nuts3745 <japanbik@gmail.com>
2022-10-16 12:26:41 +0900

    アクセスログを表示する

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use axum::{routing::get, Router};
-use std::net::SocketAddr;
+use std::{net::SocketAddr, env};
 
 #[tokio::main]
 async fn main() {
+    // logging
+    let log_level = env::var("RUST_LOG").unwrap_or("info".to_string());
+    env::set_var("RUST_LOG", log_level);
+    tracing_subscriber::fmt::init();
+
+    // api
     let app = Router::new().route("/", get(root));
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
