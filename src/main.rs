46084377121bd428d3531e7f7caec9e986fda18d
nuts3745 <japanbik@gmail.com>
2022-10-16 17:55:44 +0900

    GETとPOSTのAPIの例

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use axum::{routing::get, Router};
-use std::{net::SocketAddr, env};
+use axum::{
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use hyper::StatusCode;
+use serde::{Deserialize, Serialize};
+use std::{env, net::SocketAddr};
 
 #[tokio::main]
 async fn main() {
@@ -9,8 +15,10 @@ async fn main() {
     tracing_subscriber::fmt::init();
 
     // api
-    let app = Router::new().route("/", get(root));
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/users", post(create_user));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     tracing::debug!("listening on {}", addr);
 
     axum::Server::bind(&addr)
@@ -21,4 +29,24 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, World!"
+}
+
+async fn create_user(Json(payload): Json<CreateUser>) -> impl IntoResponse {
+    let user = User {
+        id: 1337,
+        username: payload.username,
+    };
+
+    (StatusCode::CREATED, Json(user))
+}
+
+#[derive(Deserialize)]
+struct CreateUser {
+    username: String,
+}
+
+#[derive(Serialize)]
+struct User {
+    id: u64,
+    username: String,
 }
