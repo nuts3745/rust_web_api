b8738506e88634f223ff190b92ae675946827dd5
nuts3745 <japanbik@gmail.com>
2022-11-02 08:19:45 +0000

    handlers.rsを整理した

--- backend/src/handlers.rs
+++ backend/src/handlers.rs
@@ -1,16 +1,13 @@
-use std::sync::Arc;
-
 use axum::{
     async_trait,
-    extract::{Extension, FromRequest, Path, RequestParts},
-    http::StatusCode,
-    response::IntoResponse,
+    extract::{FromRequest, RequestParts},
     BoxError, Json,
 };
+use hyper::StatusCode;
 use serde::de::DeserializeOwned;
 use validator::Validate;
-
-use crate::repositories::{CreateTodo, TodoRepository, UpdateTodo};
+pub mod label;
+pub mod todo;
 
 #[derive(Debug)]
 pub struct ValidatedJson<T>(T);
@@ -36,54 +33,4 @@ where
         })?;
         Ok(ValidatedJson(value))
     }
-}
-
-pub async fn create_todo<T: TodoRepository>(
-    ValidatedJson(payload): ValidatedJson<CreateTodo>,
-    Extension(repository): Extension<Arc<T>>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let todo = repository
-        .create(payload)
-        .await
-        .or(Err(StatusCode::NOT_FOUND))?;
-
-    Ok((StatusCode::CREATED, Json(todo)))
-}
-
-pub async fn find_todo<T: TodoRepository>(
-    Path(id): Path<i32>,
-    Extension(repository): Extension<Arc<T>>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let todo = repository.find(id).await.or(Err(StatusCode::NOT_FOUND))?;
-    Ok((StatusCode::OK, Json(todo)))
-}
-
-pub async fn all_todo<T: TodoRepository>(
-    Extension(repository): Extension<Arc<T>>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let todo = repository.all().await.unwrap();
-    Ok((StatusCode::OK, Json(todo)))
-}
-
-pub async fn update_todo<T: TodoRepository>(
-    Path(id): Path<i32>,
-    ValidatedJson(payload): ValidatedJson<UpdateTodo>,
-    Extension(repository): Extension<Arc<T>>,
-) -> Result<impl IntoResponse, StatusCode> {
-    let todo = repository
-        .update(id, payload)
-        .await
-        .or(Err(StatusCode::NOT_FOUND))?;
-    Ok((StatusCode::CREATED, Json(todo)))
-}
-
-pub async fn delete_todo<T: TodoRepository>(
-    Path(id): Path<i32>,
-    Extension(repository): Extension<Arc<T>>,
-) -> StatusCode {
-    repository
-        .delete(id)
-        .await
-        .map(|_| StatusCode::NO_CONTENT)
-        .unwrap_or(StatusCode::NOT_FOUND)
 }
---
+++ backend/src/handlers/todo.rs
@@ -0,0 +1,59 @@
+use super::ValidatedJson;
+use crate::repositories::todo::{CreateTodo, TodoRepository, UpdateTodo};
+use axum::{
+    extract::{Extension, Path},
+    http::StatusCode,
+    response::IntoResponse,
+    Json,
+};
+use std::sync::Arc;
+
+pub async fn create_todo<T: TodoRepository>(
+    ValidatedJson(payload): ValidatedJson<CreateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .create(payload)
+        .await
+        .or(Err(StatusCode::NOT_FOUND))?;
+
+    Ok((StatusCode::CREATED, Json(todo)))
+}
+
+pub async fn find_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository.find(id).await.or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn all_todo<T: TodoRepository>(
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository.all().await.unwrap();
+    Ok((StatusCode::OK, Json(todo)))
+}
+
+pub async fn update_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    ValidatedJson(payload): ValidatedJson<UpdateTodo>,
+    Extension(repository): Extension<Arc<T>>,
+) -> Result<impl IntoResponse, StatusCode> {
+    let todo = repository
+        .update(id, payload)
+        .await
+        .or(Err(StatusCode::NOT_FOUND))?;
+    Ok((StatusCode::CREATED, Json(todo)))
+}
+
+pub async fn delete_todo<T: TodoRepository>(
+    Path(id): Path<i32>,
+    Extension(repository): Extension<Arc<T>>,
+) -> StatusCode {
+    repository
+        .delete(id)
+        .await
+        .map(|_| StatusCode::NO_CONTENT)
+        .unwrap_or(StatusCode::NOT_FOUND)
+}
